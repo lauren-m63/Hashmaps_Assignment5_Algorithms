2cfb97e7af89b489b482a3ba48d6a9551b6377ef
lauren-m63 <lauren.m63@yahoo.com>
2026-03-27 13:55:52 -0500

    figuring stuff out

---
+++ src/PalindromeTriplets_6.java
@@ -0,0 +1,16 @@
+public class PalindromeTriplets_6 {
+
+
+    /*
+    6 (code) Palindrome Triplets [15 points] Given a String array of n unique words.
+A palindrome triplet is a triplet of integers (i, j, k) such that:
+• 0 ≤ i, j, k < words.length,
+• 𝑖 ≠ 𝑗, 𝑗 ≠ 𝑘, 𝑖 ≠ 𝑘,
+• words[i] + words[j] + words [k] (the concatenation of the three strings) is a palindrome.
+Return an array of all the palindrome triplets of words as lists of three integers in the form {i, j, k}
+
+Example 1: Input: words = ["race", "car", "ecar", ""] Output: [ {3, 1, 2} , {1, 2, 3} , {3, 1, 2} , {3, 0, 1} , {0, 1, 3}]
+Constraints: • 1 ≤ n ≤ 300 • 0 ≤ s.length ≤ 300 • s contains only lowercase and uppercase should be treated as the same letter.
+     */
+
+}
---
+++ src/RadixSort_5.java
@@ -0,0 +1,11 @@
+public class RadixSort_5 {
+
+    /*
+    5 (code) Radix Sort [10 points] Write a program that takes in an array of Strings and uses radix sort to sort the array lexicographically.
+Input Format An array of string s. where s[i] will be a string and s can be empty. Constraints • 1 ≤ |𝑠| ≤ 10!, where |𝒔| is the length of the strings
+array
+You are allowed to use the HashMap collection Output Format The sorted array + print the sorted array.
+Sample Input ["google", "gojo", "amazingly", "jogo", "luna", "pup", "solas", "solo", "pupperino", "amaterasu", "amazon", "puppy", "hydra", "amazonia", "vueltiao"]
+Sample Output ama, amaterasu, amazingly , amazon, amazonia, gojo, google, hydra, jogo, luna, pupperino, puppy
+     */
+}
