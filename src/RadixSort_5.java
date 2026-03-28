2cfb0ab9bbdb963d4af6b2892e6320709f4e4d6a
lauren-m63 <lauren.m63@yahoo.com>
2026-03-27 19:51:46 -0500

    notes snd starting 5

--- src/RadixSort_5.java
+++ src/RadixSort_5.java
@@ -8,4 +8,35 @@ You are allowed to use the HashMap collection Output Format The sorted array + p
 Sample Input ["google", "gojo", "amazingly", "jogo", "luna", "pup", "solas", "solo", "pupperino", "amaterasu", "amazon", "puppy", "hydra", "amazonia", "vueltiao"]
 Sample Output ama, amaterasu, amazingly , amazon, amazonia, gojo, google, hydra, jogo, luna, pupperino, puppy
      */
-}
+
+
+    /*
+    okays oi have array of strings and i put them alphabetically like the strings as a whole word i just put the words in lapahbeticla order
+    i use hashmap and its just a b so kind of like the leetcode one where letters are numbers can i just use that thing a-'c'
+
+    radix sort is doing the one character at a time  by either starting at least or first characyer and going towards front of worf
+
+    all of them diff lengths though soo do like a bit thing like -1 in between the words to know when stop
+    or null whatever so you want to start from end of array and work towards front and u know you hti the end if you hit null
+
+    you can create buckets hooray and i f it starts with like a put in a bucket then sort each of those buckets also because multiple a but ab vs az
+
+
+
+     */
+
+
+    public static String[] radixSort(String[] arr) {
+        if (arr == null || arr.length == 0) {
+            return arr;
+        }
+
+
+        return arr;
+    }
+
+
+
+
+
+} //LAST BRACKET
